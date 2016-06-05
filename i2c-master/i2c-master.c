d7be3d7fc1c5e18dd2be3b3af4a1b206a4ad2b33
Sami Kujala <skujala@iki.fi>
2016-06-05 14:49:08 +0300

    Trying different approach

--- i2c-master/i2c-master.c
+++ i2c-master/i2c-master.c
@@ -1,23 +1,6 @@
 /*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
- * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
- * Modified by Fernando Cortes <fermando.corcam@gmail.com>
- * modified by Guillermo Rivera <memogrg@gmail.com>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2016 Sami Kujala <skujala@iki.fi>
+ * 
  */
 
 #include <libopencm3/stm32/rcc.h>
@@ -25,56 +8,111 @@
 #include <libopencm3/stm32/i2c.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/f4/nvic.h>
+#include <libopencm3/stm32/f4/rcc.h>
 
+#define I2C_EV1_SLAVE_RECEIVER     ((uint32_t)0x00020002)
+#define I2C_EV1_SLAVE_TRANSMITTER  ((uint32_t)0x00060082)
+
+#define I2C_EV2                    ((uint32_t)0x00020040)
+#define I2C_EV3_BYTE_TRANSMITTED   ((uint32_t)0x00060084)
+#define I2C_EV3_BYTE_TRANSMITTING  ((uint32_t)0x00060080)
+
+#define I2C_EV3_2                  ((uint32_t)0x00000400)
+#define I2C_EV4                    ((uint32_t)0x00000010)
+#define I2C_EV5                    ((uint32_t)0x00030001)
+#define I2C_EV6_MASTER_TRANSMITTER ((uint32_t)0x00070082)
+#define I2C_EV6_MASTER_RECEIVER    ((uint32_t)0x00030002)
+
+#define I2C_EV7                    ((uint32_t)0x00030040)
+#define I2C_EV7_1
+#define I2C_EV8                    ((uint32_t)0x00070080)
+#define I2C_EV8_1                 
+#define I2C_EV8_2                 ((uint32_t)0x00070084)
+#define I2C_EV9                    ((uint32_t)0x00030008)
+
+
+#define I2C_EV5_MASTER_MODE_STARTED \
+  ((uint32_t) (I2C_SR1_SB) | (I2C_SR2_BUSY | I2C_SR2_MSL) << 16)
+
+#define I2C_EV6_MASTER_TRANSMITTER_ADDR_SENT \
+  ((uint32_t)(I2C_SR1_ADDR | I2C_SR1_TxE) | (I2C_SR2_TRA | I2C_SR2_BUSY | I2C_SR2_MSL) << 16)
+
+#define I2C_EV8_MASTER_TRANSMITTING_BYTE \
+  ((uint32_t)(I2C_SR1_TxE) | (I2C_SR2_TRA | I2C_SR2_BUSY | I2C_SR2_MSL) << 16)
+
+#define I2C_EV8_2_MASTER_BYTE_TRANSMITTED \
+  ((uint32_t)(I2C_SR1_TxE | I2C_SR1_BTF) | (I2C_SR2_TRA | I2C_SR2_BUSY | I2C_SR2_MSL) << 16)
+
+
+typedef enum {
+  SUCCESS,
+  FAILURE
+} result_t;
+
+
+uint32_t last_event = 0;
+
+uint8_t msg[5];
+uint8_t *pmsg = &msg[0];
+uint8_t msglen;
 
 static volatile uint8_t error = 0;
 
 
-static void clock_setup(void)
+
+
+static void nvic_setup(void)
 {
-//  rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_84MHZ]);
+  nvic_enable_irq(NVIC_I2C1_EV_IRQ);
+  nvic_enable_irq(NVIC_I2C1_ER_IRQ);
+}
+
+
+static void rcc_setup(void)
+{
+  rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_84MHZ]);
   
-        /* Enable GPIOD clock for LED & USARTs. */
-        rcc_periph_clock_enable(RCC_GPIOA);
-        rcc_periph_clock_enable(RCC_GPIOB);
+  /* Enable GPIOD clock for LED & USARTs. */
+  rcc_periph_clock_enable(RCC_GPIOA);
+  rcc_periph_clock_enable(RCC_GPIOB);
 
 
-        /* Enable clocks for USART2. */
-        rcc_periph_clock_enable(RCC_USART2);
+  /* Enable clocks for USART2. */
+  rcc_periph_clock_enable(RCC_USART2);
         
-        /* Enable clocks from I2C1 */
-      	rcc_periph_clock_enable(RCC_I2C1);
-      	rcc_periph_clock_enable(RCC_GPIOB);
+  /* Enable clocks from I2C1 */
+  rcc_periph_clock_enable(RCC_I2C1);
+  rcc_periph_clock_enable(RCC_GPIOB);
 }
 
 static void usart_setup(void)
 {
-        /* Setup USART2 parameters. */
-        usart_set_baudrate(USART2, 115200);
-        usart_set_databits(USART2, 8);
-        usart_set_stopbits(USART2, USART_STOPBITS_1);
-        usart_set_mode(USART2, USART_MODE_TX);
-        usart_set_parity(USART2, USART_PARITY_NONE);
-        usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+  /* Setup USART2 parameters. */
+  usart_set_baudrate(USART2, 115200);
+  usart_set_databits(USART2, 8);
+  usart_set_stopbits(USART2, USART_STOPBITS_1);
+  usart_set_mode(USART2, USART_MODE_TX);
+  usart_set_parity(USART2, USART_PARITY_NONE);
+  usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
 
-        /* Finally enable the USART. */
-        usart_enable(USART2);
+  /* Finally enable the USART. */
+  usart_enable(USART2);
 }
 
 static void gpio_setup(void)
 {
-        /* Setup GPIO pin GPIO5 on GPIO port A for LED. */
-        gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+  /* Setup GPIO pin GPIO5 on GPIO port A for LED. */
+  gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
 
-        /* Setup GPIO pins for USART2 transmit. */
-        gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+  /* Setup GPIO pins for USART2 transmit. */
+  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
 
-        /* Setup USART2 TX pin as alternate function. */
-        gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+  /* Setup USART2 TX pin as alternate function. */
+  gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
 
-      	/* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. */
-      	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8 | GPIO9);
-      	gpio_set_af(GPIOB, GPIO_AF4, GPIO8 | GPIO9);
+  /* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. */
+  gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8 | GPIO9);
+  gpio_set_af(GPIOB, GPIO_AF4, GPIO8 | GPIO9);
 }
 
 
@@ -98,8 +136,9 @@ static void i2c_setup(void)
   
   i2c_set_dutycycle(I2C1, I2C_CCR_DUTY_DIV2);
   
-  /* Enable events interrupts */
-  i2c_enable_interrupt(I2C1, I2C_CR2_ITERREN);
+  /* Enable events and errors interrupts */
+  //i2c_enable_interrupt(I2C1, I2C_CR2_ITERREN);
+  i2c_enable_interrupt(I2C1, I2C_CR2_ITEVTEN);
   
   /* 
      NXP PCA9685 datasheet specifies maximum rise time to be 1000 ns, giving
@@ -107,22 +146,21 @@ static void i2c_setup(void)
      1000 ns / T_PCLK1 = 8 (+ 1) = _9_ 
    */
   i2c_set_trise(I2C1, 8 + 1);
-
-
-	i2c_peripheral_enable(I2C1);
+  
+  i2c_peripheral_enable(I2C1);
 }
 
 
-static void WaitSR1FlagsSet (uint32_t Flags)
+static void WaitForEvent(uint32_t event) 
 {
-  while(((I2C1_SR1) & Flags) != Flags) {
+  uint32_t j;
     
-    if (error != 0) {
-      usart_send_blocking(USART2, '!');
-    }
+  while(last_event != event) {
+    GPIOA_ODR ^= GPIO5|GPIO1;
     
-    asm("nop");
-  }
+    for(j=0;j<500000;j++)
+      asm("nop");
+  }    
 }
 
 
@@ -131,46 +169,76 @@ void i2c1_er_isr(void)
   error = I2C1_SR1 & (I2C_SR1_AF | I2C_SR1_BERR | I2C_SR1_ARLO | I2C_SR1_OVR);
 }
 
+void i2c1_ev_isr(void)
+{
+  #define FLAG_MASK ((uint32_t)0x00FFFFFF)  /*<! I2C FLAG mask */
+    
+  uint32_t volatile flag1 = 0, flag2 = 0;  
+    
+  flag1 = I2C1_SR1;
+  
+  if(I2C1_SR1 & I2C_SR1_SB) {
+    i2c_enable_interrupt(I2C1, I2C_CR2_ITBUFEN);
+    GPIOA_ODR ^= GPIO5|GPIO1;
+  }
+  
+
+  /* Only read SR2 if ADDR bit is set as per documentation */
+  if(flag1 & I2C_SR1_ADDR) {
+    flag2 = I2C1_SR2;
+    flag2 = flag2 << 16; 
+  }
+    
+  last_event = (flag1 | flag2) & FLAG_MASK;
+}
+
+
 int main(void)
 {
-  uint8_t i;
   uint32_t j;
   
-	clock_setup();
+	rcc_setup();
+  nvic_setup();
 	gpio_setup();
 	usart_setup();
 	i2c_setup();
 
-  while(1) {
-//    for (i = 50; i < 80; i++) {
-      i2c_send_start(I2C1);
-      usart_send_blocking(USART2, '.');
-      WaitSR1FlagsSet(I2C_SR1_SB);
-      
-      i2c_send_7bit_address(I2C1, 0x60, 0);
-      usart_send_blocking(USART2, '.');
-      WaitSR1FlagsSet(I2C_SR1_ADDR);
-      
-      i2c_send_stop(I2C1);
-      usart_send_blocking(USART2, '.');
-
-
-      
-      // No errors mean slave responded
-      if(error == 0) {
-        usart_send_blocking(USART2, ' ');
-        usart_send_blocking(USART2, 'O');
-        usart_send_blocking(USART2, 'K');
-      } else {
-        usart_send_blocking(USART2, '!');
-        usart_send_blocking(USART2, 'O');
-        usart_send_blocking(USART2, 'K');        
-      }
+    usart_send_blocking(USART2, '.');
     
-      // Otherwise wait
-      for (j=0; j < 5000000; j++){
-        asm("nop");
-      }
+    i2c_send_start(I2C1);
+    
+    usart_send_blocking(USART2, '.');
+    WaitForEvent(I2C_EV5_MASTER_MODE_STARTED); /* EV5 */
+
+    usart_send_blocking(USART2, '.');
+      
+    i2c_send_7bit_address(I2C1, 0x60, 0);
+    WaitForEvent(I2C_EV6_MASTER_TRANSMITTER_ADDR_SENT); /* EV6 -- address sent*/
+    
+    usart_send_blocking(USART2, '.');
+        
+    i2c_send_data(I2C1, 0x00); // MODE1
+    WaitForEvent(I2C_EV8_2_MASTER_BYTE_TRANSMITTED);
+    
+    i2c_send_data(I2C1, 0x80); // RESET
+    WaitForEvent(I2C_EV8_2_MASTER_BYTE_TRANSMITTED);
+      
+    i2c_send_stop(I2C1);
+    usart_send_blocking(USART2, '.');
+      
+    // No errors mean slave responded
+    if(error == 0) {
+      usart_send_blocking(USART2, ' ');
+      usart_send_blocking(USART2, 'O');
+      usart_send_blocking(USART2, 'K');
+    } else {
+      usart_send_blocking(USART2, '!');
+      usart_send_blocking(USART2, 'O');
+      usart_send_blocking(USART2, 'K');        
+    }
+    
+  while(1) {
+      asm("nop");
   }
    
 	return 0;
